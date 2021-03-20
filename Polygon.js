7265ac7c82b3df9dbb6ef8f248b5a9f1e11af352
arihan62 <78147680+arihan62@users.noreply.github.com>
2021-03-20 15:10:30 +0530

    Add files via upload

---
+++ Polygon.js
@@ -0,0 +1,13 @@
+class Polygon {
+    constructor(x, y) {
+     super(x,y,50,50);
+     this.image=loadImage("sprites/Polygon.png")
+     
+      
+
+    }
+    display(){
+      super.display();
+    }
+    
+}
